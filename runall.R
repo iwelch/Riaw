8f415babac1b4a45bb637912a568865d049ac10a
ivo welch <ivo.welch@gmail.com>
2026-02-10 12:24:23 -0800

    Add Perl script support to runall(), warn about external scripts

--- runall.R
+++ runall.R
@@ -5,14 +5,37 @@
 
 iaw$runall <- function() {
   message( getwd() )
-  filenames <- Sys.glob("[0-9]*.R")
-  message( paste(filenames) )
-  for (fnm in filenames) {
+  rfiles <- Sys.glob("[0-9]*.R")
+  plfiles <- Sys.glob("[0-9]*.pl")
+  
+  if (length(plfiles) > 0) {
+    warning("Some scripts are external (Perl): ", paste(plfiles, collapse = ", "))
+  }
+  
+  ## Combine and sort by numeric prefix
+  allfiles <- c(rfiles, plfiles)
+  allfiles <- allfiles[order(as.numeric(sub("^([0-9]+).*", "\\1", allfiles)))]
+  
+  message( paste(allfiles, collapse = " ") )
+  
+  for (fnm in allfiles) {
     msgboth("Running ", fnm)
-    result <- tryCatch({
-      base::source(fnm)
-      NULL
-    }, error = function(e) e)
+    
+    if (grepl("\\.pl$", fnm)) {
+      ## Perl script
+      result <- tryCatch({
+        retcode <- system(paste("perl", shQuote(fnm)))
+        if (retcode != 0) stop("Perl script exited with code ", retcode)
+        NULL
+      }, error = function(e) e)
+    } else {
+      ## R script
+      result <- tryCatch({
+        base::source(fnm)
+        NULL
+      }, error = function(e) e)
+    }
+    
     if (inherits(result, "error")) {
       message("\n*** FAILED: ", fnm, " ***")
       message("Error: ", conditionMessage(result))
